0bd70fb050535afbc0fcc21784b3ad24672de34e
MarcelvanWilgenburg <32958110+MarcelvanWilgenburg@users.noreply.github.com>
2017-11-23 12:56:23 +0100

    restore

--- LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/MainApp.java
+++ LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/MainApp.java
@@ -1,6 +1,7 @@
 package com.mycompany.lostbagagesystem;
 
 import java.io.File;
+import java.util.Locale;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
@@ -9,10 +10,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.stream.events.StartElement;
 
 public class MainApp extends Application {
-    
+
     private static final FileChooser FILE_CHOOSER = new FileChooser();
 
     @Override
@@ -43,14 +47,25 @@ public class MainApp extends Application {
     }
 
     public static File selectFileToSave() {
-       
-        File selceltedFile = FILE_CHOOSER.showSaveDialog(null);
 
+        
+
+        
+        FileFilter pdfFileFilter = new FileNameExtensionFilter("PDF File", "pdf");
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.addChoosableFileFilter(pdfFileFilter);
+        
+
+        
+
+        
+        
+        File selceltedFile = FILE_CHOOSER.showSaveDialog(null);
         return selceltedFile;
     }
 
     public static File selectFileToOpen() {
-        
+
         File selceltedFile = FILE_CHOOSER.showOpenDialog(null);
 
         return selceltedFile;
