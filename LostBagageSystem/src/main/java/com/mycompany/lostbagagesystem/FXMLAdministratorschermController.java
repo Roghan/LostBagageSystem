2714911ce18fb2c2113ae471e805e3689cdfcca9
YouriPel <33253503+YouriPel@users.noreply.github.com>
2017-11-24 14:56:18 +0100

    edit

--- LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/FXMLAdministratorschermController.java
+++ LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/FXMLAdministratorschermController.java
@@ -1,25 +1,13 @@
 package com.mycompany.lostbagagesystem;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import com.mycompany.lostbagagesystem.User;
 
@@ -41,15 +29,15 @@ public class FXMLAdministratorschermController {
 //    @FXML
 //    private Button Verwijderen;
 
-//    @FXML
-//    private void zoekMedewerkerField(ActionEvent event) {
-//
-//    }
+    @FXML
+    private void zoekMedewerkerField(ActionEvent event) {
 
-//    @FXML
-//    private void handleAdd(ActionEvent event) {
-//        userList.add(new User(userIdCounter++, "?", "?", false));
-//    }
+    }
+
+    @FXML
+    private void handleAdd(ActionEvent event) {
+        userList.add(new User(userIdCounter++, "?", "?", false));
+    }
     
     @FXML
     private void handleDelete(ActionEvent event) {
