af5989ceedf560c27216b086af1aff8180611993
Ryukishi <donnyschaafs@gmail.com>
2017-11-27 11:20:45 +0100

    Ophalen
    De nieuwste versie van de Master ophalen

--- LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/MedewerkersschermController.java
+++ LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/MedewerkersschermController.java
@@ -5,9 +5,18 @@
  */
 package com.mycompany.lostbagagesystem;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,9 +24,28 @@ import javafx.fxml.Initializable;
  * @author dono
  */
 public class MedewerkersschermController implements Initializable {
+    
+    @FXML
+    private Button schadeformbtn;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) 
+                          throws IOException {
+    Stage stage; 
+    Parent root;
+    stage = new Stage();
+    root = FXMLLoader.load(getClass().getResource("/fxml/Schadeformulier.fxml"));
+    stage.setScene(new Scene(root));
+    stage.initModality(Modality.APPLICATION_MODAL);
+    stage.initOwner(schadeformbtn.getScene().getWindow());
+    stage.showAndWait();
+    }
+    
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
