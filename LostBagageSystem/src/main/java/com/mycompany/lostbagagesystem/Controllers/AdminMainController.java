0607bd32d7d11556201454a74dd9865a176515d0
YouriPel <33253503+YouriPel@users.noreply.github.com>
2017-11-28 22:33:11 +0100

    database conectie testdb

--- LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/AdminMainController.java
+++ LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/AdminMainController.java
@@ -1,6 +1,7 @@
 package com.mycompany.lostbagagesystem.Controllers;
 
 import com.mycompany.lostbagagesystem.classes.ConnectDB;
+import com.mycompany.lostbagagesystem.models.DbNaam;
 import javafx.scene.control.TableView;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
@@ -10,6 +11,8 @@ import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -23,9 +26,11 @@ public class AdminMainController implements Initializable {
     @FXML
     private TableView table;
 
-    private ObservableList<User> userList;
+//    private ObservableList<User> userList;
 
-    private int userIdCounter = 0;
+    private ObservableList<DbNaam> dbNaam;
+
+//    private int userIdCounter = 0;
 
 //    @FXML
 //    private Button Blokkeren;
@@ -39,53 +44,79 @@ public class AdminMainController implements Initializable {
 
     @FXML
     private void handleAdd(ActionEvent event) {
-        userList.add(new User(userIdCounter++, "?", "?", false));
+//        dbNaam.add(new DbNaam(userIdCounter++, "Acountnaam", "Voornaam", "Achternaam"));
     }
 
     @FXML
     private void handleDelete(ActionEvent event) {
-        User user = (User) table.getSelectionModel().getSelectedItem();
+        DbNaam user = (DbNaam) table.getSelectionModel().getSelectedItem();
 
-        userList.remove(user);
+        dbNaam.remove(user);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        userList = FXCollections.observableArrayList();
 
-        userList.add(new User(userIdCounter++, "Lennard", "Ooit", true));
-        userList.add(new User(userIdCounter++, "Peter", "Morgen", true));
-        userList.add(new User(userIdCounter++, "Claar", "Gister", true));
+        try {
+            dbTableFill();
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminMainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void dbTableFill() throws SQLException{
+        
+        ConnectDB db = new ConnectDB("fystestdb");
+
+        int id;
+        String acountnaam;
+        String wachtwoord;
+        String voornaam;
+        String achternaam;
+        String geboortedatum;
+        String postcode;
+        String huisnummer;    
+        String telefoonnummer;
+        int manVrouw;
+        int rol;    
+        int blok;
+        
+        ResultSet resultSet;
+
+        dbNaam = FXCollections.observableArrayList();
+
+        resultSet = db.executeResultSetQuery("SELECT `id`, `acountnaam`, "
+                + "`wachtwoord`, `voornaam`, `achternaam`, `geboortedatum`, "
+                + "`postcode`, `huisnummer`, `telefoonnummer`, `man/vrouw`, "
+                + "`rol`, `blok` FROM `gebruiker`");
+
+        while (resultSet.next()) {
+            id = resultSet.getInt("id");
+            acountnaam = resultSet.getString("acountnaam");
+            wachtwoord = resultSet.getString("wachtwoord");
+            voornaam = resultSet.getString("voornaam");
+            achternaam = resultSet.getString("achternaam");
+            geboortedatum = resultSet.getString("geboortedatum");
+            postcode = resultSet.getString("postcode");
+            huisnummer = resultSet.getString("huisnummer");
+            telefoonnummer = resultSet.getString("telefoonnummer");
+            manVrouw = resultSet.getInt("man/vrouw");
+            rol = resultSet.getInt("rol");
+            blok = resultSet.getInt("blok");
+            dbNaam.add(new DbNaam(id, acountnaam, wachtwoord, 
+            voornaam, achternaam, geboortedatum, 
+            postcode, huisnummer, telefoonnummer, 
+            manVrouw, rol, blok));
+        }
 
         for (int i = 0; i < table.getColumns().size(); i++) {
             TableColumn column = (TableColumn) table.getColumns().get(i);
             column.setCellValueFactory(new PropertyValueFactory(column.getId()));
         }
 
-        table.setItems(userList);
-
+        table.setItems(dbNaam);        
+        
     }
     
-    public void table() throws SQLException{
-                ConnectDB db = new ConnectDB("fystestdb");
-
-        int id;
-        String voornaam;
-        String achternaam;
-                //show results
-        ResultSet resultSet;
-
-            resultSet = db.executeResultSetQuery("SELECT `id`, `voornaam`, `achternaam` FROM `gebruiker`");
-
-
-            while (resultSet.next()) {
-                id = resultSet.getInt("id");
-                voornaam = resultSet.getString("voornaam");
-                achternaam = resultSet.getString("achternaam");
-                
-                
-                System.out.printf("%d = %s %s%n", id, voornaam, achternaam);
-            }
-    }
-
 }
