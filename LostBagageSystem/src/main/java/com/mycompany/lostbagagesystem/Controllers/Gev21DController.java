e6d0924d9fb1b0dc5a24593f76bb2fcfee13c1e7
KoenTuin <33253619+KoenTuin@users.noreply.github.com>
2017-12-05 11:54:29 +0100

    Tabel 21 dage gevonden gelinkt aan database

--- LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/Gev21DController.java
+++ LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/Gev21DController.java
@@ -1,16 +1,21 @@
 package com.mycompany.lostbagagesystem.Controllers;
 
+import com.mycompany.lostbagagesystem.classes.ConnectDB;
 import javafx.scene.control.TableView;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
-import com.mycompany.lostbagagesystem.models.User;
+import com.mycompany.lostbagagesystem.models.bagageTabel;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
@@ -23,47 +28,102 @@ public class Gev21DController implements Initializable {
 
     @FXML
     private TableView table;
+    private ObservableList<bagageTabel> bagagetabel;
+//    private ObservableList<User> userList;
 
-    private ObservableList<User> userList;
-
-    private int userIdCounter = 0;
-    @FXML
-    private TableColumn<?, ?> id;
-    @FXML
-    private TableColumn<?, ?> werknemer;
-    @FXML
-    private TableColumn<?, ?> locatie;
-    @FXML
-    private TableColumn<?, ?> actief;
-    
+//    private int userIdCounter = 0;
+//    @FXML
+//    private TableColumn<?, ?> id;
+//    @FXML
+//    private TableColumn<?, ?> werknemer;
+//    @FXML
+//    private TableColumn<?, ?> locatie;
+//    @FXML
+//    private TableColumn<?, ?> actief;
     @FXML
     private AnchorPane gev21D;
 
-    private void handleAdd(ActionEvent event) {
-        userList.add(new User(userIdCounter++, "?", "?", false));
-    }
-
-    private void handleDelete(ActionEvent event) {
-        User user = (User) table.getSelectionModel().getSelectedItem();
-
-        userList.remove(user);
-    }
-
+//    private void handleAdd(ActionEvent event) {
+//        userList.add(new User(userIdCounter++, "?", "?", false));
+//    }
+//    private void handleDelete(ActionEvent event) {
+//        User user = (User) table.getSelectionModel().getSelectedItem();
+//
+//        userList.remove(user);
+//    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        userList = FXCollections.observableArrayList();
+        try {
+            dbTableFill();
+        } catch (SQLException ex) {
+            Logger.getLogger(Gev21DController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        getChart();
+        
+   
 
-        userList.add(new User(userIdCounter++, "Youri", "Ooit", true));
-        userList.add(new User(userIdCounter++, "Peter", "Morgen", true));
-        userList.add(new User(userIdCounter++, "Claar", "Gister", true));
+    }
+   
+    @FXML
+    public void dbTableFill() throws SQLException {
+
+        ConnectDB db = new ConnectDB("fystestdb");
+
+        String registration_nr;
+        String date_found;
+        String time_found;
+        String luggage_type;
+        String brand;
+        String arrived_with_flight;
+        String luggage_tag;
+        String location_found;
+        String main_color;
+        String second_color;
+        String size;
+        String weight;
+        String passanger_name;
+        String city;
+        String other_characteristics;
+
+        ResultSet resultSet;
+
+        bagagetabel = FXCollections.observableArrayList();
+
+        resultSet = db.executeResultSetQuery("SELECT `registration_nr`, "
+                + "`date_found`,`time_found`, `luggage_type`, `brand`, "
+                + "`arrived_with_flight`, `luggage_tag`, `location_found`, "
+                + "`main_color`, `second_color`, `size`, `weight`, "
+                + "`passanger_name`, `city`, `other_characteristics` FROM `gev310sep`");
+
+        while (resultSet.next()) {
+            registration_nr = resultSet.getString("registration_nr");
+            date_found = resultSet.getString("date_found");
+            time_found = resultSet.getString("time_found");
+            luggage_type = resultSet.getString("luggage_type");
+            brand = resultSet.getString("brand");
+            arrived_with_flight = resultSet.getString("arrived_with_flight");
+            luggage_tag = resultSet.getString("luggage_tag");
+            location_found = resultSet.getString("location_found");
+            main_color = resultSet.getString("main_color");
+            second_color = resultSet.getString("second_color");
+            size = resultSet.getString("size");
+            weight = resultSet.getString("weight");
+            passanger_name = resultSet.getString("passanger_name");
+            city = resultSet.getString("city");
+            other_characteristics = resultSet.getString("other_characteristics");
+
+            bagagetabel.add(new bagageTabel(registration_nr, date_found, time_found,
+                    luggage_type, brand, arrived_with_flight, luggage_tag,
+                    location_found, main_color, second_color, size, weight, passanger_name,
+                    city, other_characteristics));
+        }
 
         for (int i = 0; i < table.getColumns().size(); i++) {
             TableColumn column = (TableColumn) table.getColumns().get(i);
             column.setCellValueFactory(new PropertyValueFactory(column.getId()));
         }
 
-        table.setItems(userList);
-        getChart();
+        table.setItems(bagagetabel);
 
     }
 
@@ -83,6 +143,7 @@ public class Gev21DController implements Initializable {
         XYChart.Series series = new XYChart.Series();
         series.setName("Bagage");
         //populating the series with data
+        
         series.getData().add(new XYChart.Data(1, 1));
         series.getData().add(new XYChart.Data(2, 2));
         series.getData().add(new XYChart.Data(3, 3));
