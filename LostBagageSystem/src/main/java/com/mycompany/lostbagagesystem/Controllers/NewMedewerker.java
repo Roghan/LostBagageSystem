55afb042bedb3a2e32d682443b5bdad3c1c30d0c
KoenTuin <33253619+KoenTuin@users.noreply.github.com>
2017-12-30 16:58:35 +0100

    done

--- LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/NewMedewerker.java
+++ LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/NewMedewerker.java
@@ -5,6 +5,7 @@ package com.mycompany.lostbagagesystem.Controllers;
 import com.mycompany.lostbagagesystem.classes.ConnectDB;
 import com.mycompany.lostbagagesystem.classes.Language;
 import java.io.IOException;
+import java.security.MessageDigest;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -59,6 +60,25 @@ public class NewMedewerker {
     }
 
     @FXML
+    public static String sha256(String base) {
+    try{
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(base.getBytes("UTF-8"));
+        StringBuffer hexString = new StringBuffer();
+
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    } catch(Exception ex){
+       throw new RuntimeException(ex);
+    }
+}
+    
+    @FXML
     public void sendtodatabase() throws IOException {
 
         ConnectDB db = new ConnectDB("fystestdb");
@@ -74,12 +94,14 @@ public class NewMedewerker {
         String manV = manVrouw.getText();
         String roll = rol.getText();
 
+        String wachtwoord1 = sha256(wachtw);
+        
         String query = String.format("INSERT INTO `gebruiker` "
                 + "(`acountnaam`, `wachtwoord`, `voornaam`, `achternaam`, "
                 + "`geboortedatum`, `postcode`, `huisnummer`, `telefoonnummer`, "
                 + "`man/vrouw`, `rol`)"
                 + " VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
-                acountN, wachtw, voorN, achterN, geboorteD, postC, huisN, telefoonN, manV, roll);
+                acountN, wachtwoord1, voorN, achterN, geboorteD, postC, huisN, telefoonN, manV, roll);
 
         int numberAffected = db.executeUpdateQuery(query);
         System.out.println(numberAffected);
