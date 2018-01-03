cc7077e9762da4645556031eb3601f29c47a6659
Roghan <33095297+Roghan@users.noreply.github.com>
2018-01-03 22:45:03 +0100

    Database update met nieuwe querry en schermen

--- LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/VermistBagFormController.java
+++ LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/VermistBagFormController.java
@@ -2,11 +2,17 @@
  */
 package com.mycompany.lostbagagesystem.Controllers;
 
+import com.mycompany.lostbagagesystem.classes.ConnectDB;
 import com.mycompany.lostbagagesystem.classes.language;
+import com.mycompany.lostbagagesystem.models.ColourPicker;
 import com.mycompany.lostbagagesystem.models.FormulierCheck;
 import com.mycompany.lostbagagesystem.models.ToggleGroupResult;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -17,7 +23,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -31,7 +39,7 @@ import javafx.stage.Stage;
 public class VermistBagFormController implements Initializable {
 
     @FXML
-    private TextField txtVliegveldID;
+    private MenuButton btnVliegveldID;
     @FXML
     private DatePicker txtDatum;
     @FXML
@@ -194,8 +202,18 @@ public class VermistBagFormController implements Initializable {
     private DatePicker txtGeboorteDatum;
     @FXML
     private ResourceBundle bundle;
+    @FXML
+    private MenuButton kleur1Menu;
+    @FXML
+    private MenuButton kleur2Menu;
+    @FXML
+    private TextField txtVan;
+    @FXML
+    private TextField txtNaar;
+    @FXML
+    private ToggleGroup IATA;
+
     private Locale locale;
-    
 
     private String time;
     private String datum;
@@ -203,9 +221,6 @@ public class VermistBagFormController implements Initializable {
     private String voorLetters;
     private String tussenVoegsel;
     private String achterNaam;
-    private String geboorteDatum;
-    private String gender;
-    private String landNaam;
     private String straatNaam;
     private String huisNummer;
     private String postCode;
@@ -213,17 +228,16 @@ public class VermistBagFormController implements Initializable {
     private String email;
     private String telefoonNummer;
     private String mobielNummer;
-    private String labelNummer;
     private String vluchtNummer;
-    private String bestemming;
     private String bagageLabel;
     private String typeBagage;
     private String merk;
     private String bijzondereOpmerking;
-
-    private String colour;
-    private String colour2;
-    private String colour3;
+    private String van;
+    private String naar;
+    private String ralcode1;
+    private String ralcode2;
+    private String iataString;
 
     @FXML
     void annuleren3(ActionEvent event) {
@@ -232,38 +246,18 @@ public class VermistBagFormController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-        
 
     }
 
     @FXML
-    public void bagageToevoegen(ActionEvent toevoegen) throws IOException {
-        Stage stage = new Stage();
-
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ToevoegenBagage.fxml"), ResourceBundle.getBundle("Bundles.Lang", language.getCurrentLocale()));
-        
-
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-
-        stage.setTitle("Bagage Toevoegen");
-        stage.setMaximized(false);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    @FXML
-    void insturen(ActionEvent insturen) {
+    void insturen(ActionEvent insturen) throws SQLException {
         System.out.println("KNOP INSTUREN INGEDRUKT");
 
         time = txtTime.getText();
-        vliegveldID = txtVliegveldID.getText();
+        datum = txtDatum.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         voorLetters = txtVoorletters.getText();
         tussenVoegsel = txtTussenvoegsel.getText();
         achterNaam = txtAchternaam.getText();
-        landNaam = txtLandnaam.getText();
-        gender = ToggleGroupResult.getPick(genderGroup);
         straatNaam = txtStraatnaam.getText();
         huisNummer = txtHuisNummer.getText();
         postCode = txtPostcode.getText();
@@ -271,23 +265,25 @@ public class VermistBagFormController implements Initializable {
         email = txtEmail.getText();
         telefoonNummer = txtTelefoon.getText();
         mobielNummer = txtMobielNummer.getText();
-        labelNummer = txtLabelNummer.getText();
         vluchtNummer = txtVluchtNummer.getText();
-        bestemming = txtBestemming.getText();
+        van = txtVan.getText();
+        naar = txtNaar.getText();
+        bagageLabel = txtBagageLabel.getText();
+        typeBagage = txtTypeBagage.getText();
+        merk = txtMerk.getText();
+        bijzondereOpmerking = txtBijzondereOpmerking.getText();
 
         TextField[] reqTextFields = new TextField[]{
             txtVluchtNummer,
-            txtLabelNummer,
             txtBestemming,
             txtVoorletters,
             txtAchternaam,
             txtStraatnaam,
             txtPostcode,
-            txtVliegveldID,
-            txtTime,
             txtWoonplaats,
             txtEmail,
-            txtLandnaam
+            txtVan,
+            txtNaar
 
         };
 
@@ -306,17 +302,108 @@ public class VermistBagFormController implements Initializable {
         TextField[] reqIntFields = new TextField[]{
             txtHuisNummer};
 
-        FormulierCheck.verification(reqTextFields, PhoneFields, datePickers, reqIntFields);
+        //boolean form = FormulierCheck.verification(reqTextFields, PhoneFields, datePickers, reqIntFields);
+        if (true) {
+            sendToDatabase();
+        }
 
     }
-    
-    private void loadLang(String lang){
+
+    private void loadLang(String lang) {
         locale = new Locale(lang);
         bundle = ResourceBundle.getBundle("Bundles.Lang", locale);
-        
-        
-        
+
     }
-    
+
+    public void sendToDatabase() throws SQLException {
+        // Making a new prepared statement 
+        PreparedStatement myStmt = null;
+        Connection conn = null;
+        ConnectDB db = new ConnectDB();
+        int numberAffected = 0;
+
+        // Updates persoonsgegevens
+        String persoonsgegevens = "INSERT INTO `klant` "
+                + "(`Voorletter`,`Tussenvoegsel`, `Achternaam`, `Straat`, `HuisNummer`, `Postcode`, "
+                + "`Woonplaats`, `Email`, `Telefoon1`, `Telefoon2`, `VluchtNummer`,"
+                + "`State`, `Date`, `Time`, `LabelNummer`, `Type`, `Brand`, `Color1`, `Color2`,"
+                + "`Characteristics`, `Luchthaven`, `VluchtNummer`, `From`, `To`) VALUES"
+                + "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try {
+            conn = db.getDBConnection();
+            myStmt = conn.prepareStatement(persoonsgegevens);
+            // Filling in the question marks from the query
+            myStmt.setString(1, voorLetters);
+            myStmt.setString(2, tussenVoegsel);
+            myStmt.setString(3, achterNaam);
+            myStmt.setString(4, straatNaam);
+            myStmt.setString(5, huisNummer);
+            myStmt.setString(6, postCode);
+            myStmt.setString(7, woonPlaats);
+            myStmt.setString(8, email);
+            myStmt.setString(9, telefoonNummer);
+            myStmt.setString(10, mobielNummer);
+            myStmt.setString(11, vluchtNummer);
+            myStmt.setString(12, "Lost");
+            myStmt.setString(13, datum);
+            myStmt.setString(14, time);
+            myStmt.setString(15, bagageLabel);
+            myStmt.setString(16, typeBagage);
+            myStmt.setString(17, merk);
+            myStmt.setString(18, ralcode1);
+            myStmt.setString(19, ralcode2);
+            myStmt.setString(20, bijzondereOpmerking);
+            myStmt.setString(21, iataString);
+            myStmt.setString(22, van);
+            myStmt.setString(23, naar);
+
+            // Execute INSERT sql statement
+            numberAffected = myStmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // Closing the prepared statement for memory purposes
+            if (myStmt != null) {
+                myStmt.close();
+            }
+            // Closing the database connection for memory purposes
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        System.out.println(numberAffected);
+
+    }
+
+    @FXML
+    public void IATACHECK(ActionEvent event) {
+        RadioMenuItem iattaItem = (RadioMenuItem) IATA.getSelectedToggle();
+        iataString = iattaItem.getText();
+        btnVliegveldID.setText(iataString);
+        System.out.println(iataString);
+
+    }
+
+    @FXML
+    public void kleurkiezer1(ActionEvent event) {
+        RadioMenuItem item = (RadioMenuItem) kleur1.getSelectedToggle();
+        String kleur = item.getText();
+        kleur1Menu.setText(kleur);
+        System.out.println(kleur);
+        ralcode1 = ColourPicker.GetColour(kleur);
+
+    }
+
+    @FXML
+    public void kleurkiezer2(ActionEvent event) {
+        RadioMenuItem item = (RadioMenuItem) kleur2.getSelectedToggle();
+        String kleur = item.getText();
+        kleur2Menu.setText(kleur);
+        System.out.println(kleur);
+        ralcode2 = ColourPicker.GetColour(kleur);
+    }
 
 }
