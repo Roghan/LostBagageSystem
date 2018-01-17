648cf8f70da18b7cd3669c247016b74f12725967
YouriPel <33253503+YouriPel@users.noreply.github.com>
2018-01-17 15:41:19 +0100

    Revert "Merge branch 'master' of https://github.com/Roghan/LostBagageSystem"
    This reverts commit cb6a3612749dba3b8781a9d7fa80afb230845bbc, reversing
    changes made to 6706b93cc5cc72204e5f00b4699a31811706f4be.

--- LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/ManMedewerkerViewController.java
+++ LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/ManMedewerkerViewController.java
@@ -1,6 +1,8 @@
 /*Controller Class voor de medewerker Table voor het ManagerScherm
- */
+*/
+
 package com.mycompany.lostbagagesystem.Controllers;
+
 
 import com.mycompany.lostbagagesystem.Controllers.ManMainController;
 import com.mycompany.lostbagagesystem.classes.ConnectDB;
@@ -11,15 +13,12 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
@@ -28,6 +27,8 @@ import javafx.scene.layout.AnchorPane;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 /**
  * FXML Controller class
  *
@@ -41,10 +42,7 @@ public class ManMedewerkerViewController implements Initializable {
     private ObservableList<DbNaam> dbNaam;
     @FXML
     private AnchorPane tablepane;
-    @FXML
-    private TextField txtZoek;
-
-    private String filter;
+    
 
     /**
      * Initializes the controller class.
@@ -57,25 +55,6 @@ public class ManMedewerkerViewController implements Initializable {
         } catch (SQLException ex) {
             Logger.getLogger(ManMainController.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        txtZoek.textProperty().addListener(new ChangeListener<String>() {
-
-            @Override
-            public void changed(ObservableValue<? extends String> observable,
-                    String oldValue, String newValue) {
-                try {
-                    if (txtZoek.getText() != null) {
-                        filter = txtZoek.getText();
-                        dbTableFillWithFilter();
-                    }
-
-                } catch (SQLException ex) {
-                    Logger.getLogger(AdminMainController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
-            }
-
-        });
 
     }
 
@@ -94,7 +73,7 @@ public class ManMedewerkerViewController implements Initializable {
         dbNaam = FXCollections.observableArrayList();
 
         resultSet = db.executeResultSetQuery("SELECT `id`, `acountnaam`, "
-                + "`wachtwoord`, `rol`, `blok` FROM `gebruiker`");
+                + "`wachtwoord`, `rol`, `blok` FROM `gebruiker` WHERE `rol` = 3");
 
         while (resultSet.next()) {
             id = resultSet.getInt("id");
@@ -103,43 +82,6 @@ public class ManMedewerkerViewController implements Initializable {
             rol = resultSet.getInt("rol");
             blok = resultSet.getInt("blok");
             dbNaam.add(new DbNaam(id, acountnaam, wachtwoord, rol, blok));
-        }
-
-        for (int i = 0; i < table.getColumns().size(); i++) {
-            TableColumn column = (TableColumn) table.getColumns().get(i);
-            column.setCellValueFactory(new PropertyValueFactory(column.getId()));
-        }
-
-        table.setItems(dbNaam);
-
-    }
-
-    public void dbTableFillWithFilter() throws SQLException {
-
-        ConnectDB db = new ConnectDB("lbs_database");
-
-        int id;
-        String acountnaam;
-        String wachtwoord;
-        int rol;
-        int blok;
-
-        ResultSet resultSet;
-
-        dbNaam = FXCollections.observableArrayList();
-
-        String query = "SELECT `id`, `acountnaam`,`wachtwoord`, `rol`, `blok` FROM `gebruiker` WHERE"
-                + "`acountnaam` LIKE '%" + filter + "%' ";
-        resultSet = db.executeResultSetQuery(query);
-
-        while (resultSet.next()) {
-            id = resultSet.getInt("id");
-            acountnaam = resultSet.getString("acountnaam");
-            wachtwoord = resultSet.getString("wachtwoord");
-            rol = resultSet.getInt("rol");
-            blok = resultSet.getInt("blok");
-            dbNaam.add(new DbNaam(id, acountnaam, wachtwoord,
-                    rol, blok));
         }
 
         for (int i = 0; i < table.getColumns().size(); i++) {
