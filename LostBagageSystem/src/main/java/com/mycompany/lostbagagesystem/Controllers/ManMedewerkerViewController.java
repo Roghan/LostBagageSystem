672448ad6322ef38fa8a74dfb9eca8a91fc3e0b3
Koen <koen.van.der.tuin@hva.nl>
2020-03-27 12:29:43 +0100

    gitnore

--- LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/ManMedewerkerViewController.java
+++ LostBagageSystem/src/main/java/com/mycompany/lostbagagesystem/Controllers/ManMedewerkerViewController.java
@@ -23,7 +23,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
+<<<<<<< HEAD
 
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+>>>>>>> parent of 648cf8f7... Revert "Merge branch 'master' of https://github.com/Roghan/LostBagageSystem"
 /**
  * FXML Controller class
  *
